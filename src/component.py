aa300448264d229140bfd47f8ac042769760d384
Josef Kudera <46950237+kudj@users.noreply.github.com>
2025-10-01 08:58:24 +0200

    ruff + cleanup

--- src/component.py
+++ src/component.py
@@ -1,7 +1,3 @@
-"""
-Template Component main class.
-
-"""
 import logging
 
 from keboola.component.base import ComponentBase
@@ -10,37 +6,24 @@ from keboola.component.exceptions import UserException
 # configuration variables
 from dbx.client import DataBricksClient
 
-KEY_API_TOKEN = '#api_token'
-KEY_BASE_URL = 'base_url'
-KEY_JOB_ID = 'job_id'
-KEY_SSL_VERIFY = 'ssl_verify'
+KEY_API_TOKEN = "#api_token"
+KEY_BASE_URL = "base_url"
+KEY_JOB_ID = "job_id"
+KEY_SSL_VERIFY = "ssl_verify"
 
 REQUIRED_PARAMETERS = [KEY_API_TOKEN, KEY_BASE_URL]
 REQUIRED_IMAGE_PARS = []
 
 
 class Component(ComponentBase):
-    """
-        Extends base class for general Python components. Initializes the CommonInterface
-        and performs configuration validation.
-
-        For easier debugging the data folder is picked up by default from `../data` path,
-        relative to working directory.
-
-        If `debug` parameter is present in the `config.json`, the default logger is set to verbose DEBUG mode.
-    """
-
     def __init__(self):
         super().__init__()
         self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        self.dbx_client = DataBricksClient(self.configuration.parameters[KEY_BASE_URL],
-                                           self.configuration.parameters[KEY_API_TOKEN],
-                                           ssl_verify=False)
+        self.dbx_client = DataBricksClient(
+            self.configuration.parameters[KEY_BASE_URL], self.configuration.parameters[KEY_API_TOKEN], ssl_verify=False
+        )
 
     def run(self):
-        """
-        Main execution code
-        """
         params = self.configuration.parameters
         self.validate_configuration_parameters([KEY_JOB_ID])
         job_id = params[KEY_JOB_ID]
@@ -49,9 +32,9 @@ class Component(ComponentBase):
         job_details = self.dbx_client.get_job_detail(job_id)
         logging.info(f"Job named '{job_details['settings']['name']}' found. Trying to run the dbx job ID: {job_id}")
         resp = self.dbx_client.run_job_now(job_id)
-        self.dbx_client.wait_for_job(resp['run_id'])
+        self.dbx_client.wait_for_job(resp["run_id"])
 
-        logging.info('Job finished successfully!')
+        logging.info("Job finished successfully!")
 
 
 """
@@ -63,7 +46,7 @@ if __name__ == "__main__":
         # this triggers the run method by default and is controlled by the configuration.action parameter
         comp.execute_action()
     except UserException as exc:
-        detail = ''
+        detail = ""
         if len(exc.args) > 1:
             detail = exc.args[1]
         logging.exception(exc, extra={"full_message": detail})
--- src/dbx/client.py
+++ src/dbx/client.py
@@ -28,11 +28,9 @@ class DataBricksClient(HttpClient):
 
         """
 
-        body = {"job_id": job_id,
-                "idempotency_token": str(uuid.uuid1())
-                }
+        body = {"job_id": job_id, "idempotency_token": str(uuid.uuid1())}
         try:
-            return self.post(endpoint_path='/api/2.1/jobs/run-now', json=body, verify=self.ssl_verify)
+            return self.post(endpoint_path="/api/2.1/jobs/run-now", json=body, verify=self.ssl_verify)
 
         except HTTPError as http_err:
             raise DataBricksClientClientException(http_err) from http_err
@@ -49,7 +47,7 @@ class DataBricksClient(HttpClient):
         """
         parameters = {"run_id": run_id}
         try:
-            return self.get(endpoint_path='/api/2.1/jobs/runs/get', params=parameters, verify=self.ssl_verify)
+            return self.get(endpoint_path="/api/2.1/jobs/runs/get", params=parameters, verify=self.ssl_verify)
         except HTTPError as http_err:
             raise DataBricksClientClientException(http_err) from http_err
 
@@ -65,14 +63,13 @@ class DataBricksClient(HttpClient):
         """
         parameters = {"job_id": job_id}
         try:
-            return self.get(endpoint_path='/api/2.1/jobs/get', params=parameters, verify=self.ssl_verify)
+            return self.get(endpoint_path="/api/2.1/jobs/get", params=parameters, verify=self.ssl_verify)
         except HTTPError as http_err:
-            raise DataBricksClientClientException(f"Failed to retrieve job ID: {job_id}. "
-                                                  f"Please check if it's correct", http_err) from http_err
+            raise DataBricksClientClientException(
+                f"Failed to retrieve job ID: {job_id}. Please check if it's correct", http_err
+            ) from http_err
 
-    def wait_for_job(self, run_id: int,
-                     timeout_seconds: float = None,
-                     poll_interval_seconds: float = None) -> dict:
+    def wait_for_job(self, run_id: int, timeout_seconds: float = None, poll_interval_seconds: float = None) -> dict:
         """
         Wait for the DBX job to finish. Raises exception when state is not SUCCESS
         Args:
@@ -87,18 +84,21 @@ class DataBricksClient(HttpClient):
         timeout_seconds = timeout_seconds or self.WAIT_TIMEOUT_SECONDS
         poll_interval_seconds = poll_interval_seconds or self.WAIT_POLL_INTERVAL_SECONDS
         expires_at = time.time() + timeout_seconds
-        exit_states = ['TERMINATED', 'SKIPPED', 'INTERNAL_ERROR']
+        exit_states = ["TERMINATED", "SKIPPED", "INTERNAL_ERROR"]
         while True:
             run_meta = self.get_job_run(run_id)
-            run_state = run_meta['state']['life_cycle_state']
+            run_state = run_meta["state"]["life_cycle_state"]
             if run_state in exit_states:
                 break
             if time.time() > expires_at:
-                raise DataBricksClientClientException(f'Timeout of {timeout_seconds} seconds reached for run {run_id}.')
+                raise DataBricksClientClientException(f"Timeout of {timeout_seconds} seconds reached for run {run_id}.")
             time.sleep(poll_interval_seconds)
 
-        if run_meta['state']['result_state'] != 'SUCCESS':
-            raise DataBricksClientClientException(f"Job execution failed with status: "
-                                                  f"{run_meta['state']['result_state']}, "
-                                                  f"Reason: {run_meta['state']['state_message']}", run_meta)
+        if run_meta["state"]["result_state"] != "SUCCESS":
+            raise DataBricksClientClientException(
+                f"Job execution failed with status: "
+                f"{run_meta['state']['result_state']}, "
+                f"Reason: {run_meta['state']['state_message']}",
+                run_meta,
+            )
         return run_meta
